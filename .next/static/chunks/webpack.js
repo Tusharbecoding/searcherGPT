a960ae1e2ed4823e45c224223c8eee28f1bf65f8
Tusharbecoding <tusharchopra02122002@gmail.com>
2024-08-29 10:14:08 +0530

    size error

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "76ffec8c91be3c52"; }
+/******/ 		__webpack_require__.h = function() { return "adc33bcbc6d70a9c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/components/ChatMessage/ChatMessage.tsx
+++ src/components/ChatMessage/ChatMessage.tsx
@@ -1,14 +1,14 @@
-import { Avatar, Button } from '@nextui-org/react'
-import clsx from 'clsx'
-import React from 'react'
-import { useAnimatedText } from '../AnimatedText'
+import { Avatar, Button } from "@nextui-org/react";
+import clsx from "clsx";
+import React from "react";
+import { useAnimatedText } from "../AnimatedText";
 
-export type ChatMessageProps = Omit<React.HTMLProps<HTMLDivElement>, 'role'> & {
-  message: string
-  role: 'user' | 'assistant'
-  disableAnimation?: boolean
-  onEdit?: () => void // New prop to handle the edit button click
-}
+export type ChatMessageProps = Omit<React.HTMLProps<HTMLDivElement>, "role"> & {
+  message: string;
+  role: "user" | "assistant";
+  disableAnimation?: boolean;
+  onEdit?: () => void; // New prop to handle the edit button click
+};
 
 export const ChatMessage: React.FC<ChatMessageProps> = ({
   message,
@@ -19,22 +19,22 @@ export const ChatMessage: React.FC<ChatMessageProps> = ({
 }) => {
   const content = useAnimatedText(message, {
     maxTime: 1000,
-    disabled: role === 'user' || disableAnimation,
-  })
+    disabled: role === "user" || disableAnimation,
+  });
 
   return (
     <div
       {...props}
-      className={clsx('relative group', props.className)} // Added 'group' class here
+      className={clsx("relative group", props.className)} // Added 'group' class here
     >
       <div className="flex flex-row gap-4 items-start">
         <Avatar
           className="flex-shrink-0"
           showFallback
-          color={role === 'assistant' ? 'primary' : 'default'}
-          name={role === 'assistant' ? 'A' : ''}
+          color={role === "assistant" ? "primary" : "default"}
+          name={role === "assistant" ? "A" : ""}
           classNames={{
-            name: 'text-[16px]',
+            name: "text-[16px]",
           }}
         />
         <div className="flex-grow border border-gray-200 rounded-lg p-4 text-md bg-white shadow-sm mt-[-4px] relative">
@@ -43,11 +43,11 @@ export const ChatMessage: React.FC<ChatMessageProps> = ({
             dangerouslySetInnerHTML={{ __html: content }}
           />
         </div>
-        {role === 'user' && (
+        {role === "user" && (
           <Button
             auto
             flat
-            size="xs"
+            size="sm"
             onClick={onEdit}
             className="opacity-0 group-hover:opacity-100 transition-opacity absolute top-1 right-2"
           >
@@ -56,5 +56,5 @@ export const ChatMessage: React.FC<ChatMessageProps> = ({
         )}
       </div>
     </div>
-  )
-}
+  );
+};
